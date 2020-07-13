9a43920f18919e5efcce323bc9638ce970583b46
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-13 16:12:18 +0300

    mine

--- node/routes.py
+++ node/routes.py
@@ -1,10 +1,37 @@
 from flask import Flask, jsonify, request
-from node import app, blockchain
+from node import app, blockchain, node_identifier
 
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # run proof of work algorithm to get the next proof
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    
+    # add new block to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    # we should receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new block.
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    response = {
+        'message': 'New Block Forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
@@ -19,7 +46,7 @@ def new_transaction():
 
     transaction_index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 
-    response = {'message': f'Transaction will be added to Block {index}'}
+    response = {'message': f'Transaction will be added to Block {transaction_index}'}
     return jsonify(response), 201
 
     return "We'll add a new transaction"
