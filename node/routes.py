cc459570c90d7d65e672ff2d40c300404ff31f53
Artyom Pugachev <artempugachev1@gmail.com>
2020-07-13 15:21:46 +0300

    new transaction method

--- node/routes.py
+++ node/routes.py
@@ -1,4 +1,4 @@
-from flask.json import jsonify
+from flask import Flask, jsonify, request
 from node import app, blockchain
 
 
@@ -9,6 +9,19 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    requeireds = ['sender', 'recipient', 'amount']
+
+    if not all(value in values for value in requeireds):
+        return 'Missing values', 400
+
+    transaction_index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
     return "We'll add a new transaction"
 
 
